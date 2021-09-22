8ef4f911126ebc4eb258fdaefb0e01890e7cb7db
Bartideusz <90344408+Bartideusz@users.noreply.github.com>
2021-09-22 11:19:35 +0200

    Add files via upload

---
+++ 22.09.2021/napis.py
@@ -0,0 +1,8 @@
+print ('programujemy \n w Pythonie')
+print ('i nie tylko')
+print ('to jest dodawanie ',23+5)
+# to jest komentarz
+# funkcja Print pozwala łączyć wyświetlanie napisów, wyrażeń i zmiennych
+# umieszczenie w tekście sekwencji znaków \n wymusza przejście do następnej linii
+# umieszczenie znaku \n na początku zdania wymusza dodatkowy odstęp miedzy linijkami
+input ('\n\n Aby zakonczyc, nacisnij ENTER')
